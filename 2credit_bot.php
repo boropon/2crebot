3753e993ee0b31979bc66b45b0b0b08c764f8444
boropon <coco_mix_620@yahoo.co.jp>
2013-07-01 00:22:34 +0900

    Fixed the determination process of a snowy day.
    Modified the contents of the tweet.
    Refactoring.

--- 2credit_bot.php
+++ 2credit_bot.php
@@ -1,73 +1,21 @@
 <?php
-	$weather_file = "weather.txt";
-	
-	$hour = date('G');
-	//朝9時に明日の天気を取得し、ファイルに保存
-	if($hour == '9') {
-		//明日の天気を取得
-		//Yahoo!のRSSから鳥取県（東部）の天気を取得
+	//引数に時間を渡すと、その時間の天気を取得する関数
+	//Yahoo!のRSSから鳥取県（東部）の天気を取得
+	function getweather($time) {
 		$url = "http://rss.weather.yahoo.co.jp/rss/days/6910.xml";
 		$xml = simplexml_load_file($url);
-		$tomorrow = date('j', strtotime("tomorrow"));
 		foreach ($xml->channel->item as $item) {
-			if (strstr($item->title, " ".$tomorrow."日")) {
+			if (strstr($item->title, " ".date('j', $time)."日")) {
 				$description = $item->description;
 			}
 		}
 		$separate_description = explode(" ", $description);
 		$tomorrow_weather = $separate_description[0];
-		
-		//天気をファイル(weather.txt)に保存
-		$file = fopen($weather_file, "w+");
-		fwrite($file, $tomorrow_weather);
+		return $tomorrow_weather;
 	}
 	
-	//朝8時に今日の音ゲー情報をツイート
-	if($hour == '8') {
-		//曜日に対応するツイートメッセージを設定
-		$dayoftheweek = date('D');
-		switch($dayoftheweek) {
-			case "Mon":
-				$dayoftheweekjap = "（月）";
-				$daily_message = "「ぽくぽく」「ぎたー」「たいこ」が100円2クレ";
-				break;
-			case "Tue":
-				$dayoftheweekjap = "（火）";
-				$daily_message = "「びーまに」がパセリ料金40%ダウン";
-				break;
-			case "Wed":
-				$dayoftheweekjap = "（水）";
-				$daily_message = "「ゆびーと」がパセリ料金40%ダウン";
-				break;
-			case "Thu":
-				$dayoftheweekjap = "（木）";
-				$daily_message = "「りふれくびーと」がパセリ料金40%ダウン";
-				break;
-			case "Fri":
-				$dayoftheweekjap = "（金）";
-				$daily_message = "「ぱかぱか」がパセリ料金40%ダウン";
-				break;
-			case "Sat":
-				$dayoftheweekjap = "（土）";
-				$daily_message = "通常営業日";
-				break;
-			case "Sun":
-				$dayoftheweekjap = "（日）";
-				$daily_message = "通常営業日";
-				break;
-		}
-		
-		//前日9時に保存した天気を参照し、雪なら雪の日イベントメッセージに上書き
-		$today_weather = file_get_contents($weather_file);
-		if (strstr($today_weather, "雪")) {
-			$daily_message = "雪の日イベント実施日です。ヽ(∀ﾟ )人(ﾟ∀ﾟ)人( ﾟ∀)ノ 音ゲー全部100円2クレ";
-		}
-		
-		//ツイートするメッセージの作成
-		$today = date('j');
-		$today_month = date('n');
-		$message = "【".$today_month."月".$today."日".$dayoftheweekjap."】 "."本日は".$daily_message."です。ゆっくりしていってね！";
-		
+	//引数に渡した文字列をツイート
+	function tweetmessage($message) {
 		//ツイートするための準備
 		require_once("twitteroauth.php");
 		$consumer_key = "epZiUS66NYxESrtIy1Cg";
@@ -77,5 +25,73 @@
 		$to = new TwitterOAuth($consumer_key,$consumer_secret,$access_token,$access_token_secret);
 		//ツイート
 		$to->OAuthRequest("https://twitter.com/statuses/update.xml","POST",array("status"=>"$message"));
+	}
+?>
+
+<?php
+	//天気を保存しておくファイル
+	$weather_file = "weather.txt";
+	
+	$hour = date('G');
+	//朝9時に明日の天気を取得し、ファイルに保存
+	if($hour == '9') {
+		//明日の天気を取得
+		$tomorrow_time = strtotime("tomorrow");
+		$tomorrow_weather = getweather($tomorrow_time);
+		
+		//天気をファイル(weather.txt)に保存
+		$file = fopen($weather_file, "w+");
+		fwrite($file, $tomorrow_weather);
+	}
+	
+	//朝8時に今日の音ゲー情報をツイート
+	if($hour == '8') {
+		//今日の曜日を確認し、曜日に対応するツイートメッセージを設定
+		$dayoftheweek = date('D');
+		switch($dayoftheweek) {
+			case "Mon":
+				$dayoftheweekjap = "月";
+				$daily_message = "「ぽくぽく」「ぎたー」「たいこ」が100円2クレ";
+				break;
+			case "Tue":
+				$dayoftheweekjap = "火";
+				$daily_message = "「びーまに」がパセリ料金40%ダウン";
+				break;
+			case "Wed":
+				$dayoftheweekjap = "水";
+				$daily_message = "「ゆびーと」がパセリ料金40%ダウン";
+				break;
+			case "Thu":
+				$dayoftheweekjap = "木";
+				$daily_message = "「りふれくびーと」がパセリ料金40%ダウン";
+				break;
+			case "Fri":
+				$dayoftheweekjap = "金";
+				$daily_message = "「ぱかぱか」がパセリ料金40%ダウン";
+				break;
+			case "Sat":
+				$dayoftheweekjap = "土";
+				$daily_message = "通常営業日";
+				break;
+			case "Sun":
+				$dayoftheweekjap = "日";
+				$daily_message = "通常営業日";
+				break;
+		}
+		
+		//前日9時に保存した天気を参照し、雪なら雪の日イベント用メッセージに変更
+		$today_weather = file_get_contents($weather_file);
+		if ($today_weather === "雪" || $today_weather === "暴風雪" || preg_match("/^(雪時々).*/", $today_weather)) {
+			$daily_message = "雪の日イベント実施日で、音ゲー全部100円2クレ";
+		}
+		
+		//ツイートするメッセージの作成（【[今日の月日]】おはようございます☆彡 本日は[音ゲー情報]です。ゆっくりしていってね！[顔文字]）
+		$today = date('j');
+		$today_month = date('n');
+		$face = array("（ ´Д｀）ｙ━─┛~~", "⊂(ﾟДﾟ⊂⌒｀つ≡≡≡", "（・∀・）", "( ◕ ‿‿ ◕ ) ", "ξ・`∀・)ξ", "(σ´∀`)σ", "(*´･ω･`)ﾉ ", "(｀・ω・´) ゞ", "|・ω・`）", "＼(●)／");
+		$today_face = $face[mt_rand(0, count($face)-1)];
+		$message = "【".$today_month."月".$today."日（".$dayoftheweekjap."）】 おはようございます☆彡 本日は".$daily_message."です。ゆっくりしていってね！".$today_face;
+		//ツイート
+		tweetmessage($message);
 	}
 ?>
